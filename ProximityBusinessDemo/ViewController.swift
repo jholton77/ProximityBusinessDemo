dcf6ad85139c38adbc7fd19f8a9a4be0b9f972cc
jholton77 <jholt45@yahoo.com>
2021-11-01 14:16:14 -0500

    Initial Commit

---
+++ ProximityBusinessDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProximityBusinessDemo
+//
+//  Created by Josh Holton on 11/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
